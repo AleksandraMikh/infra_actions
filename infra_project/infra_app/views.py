c08bec302257181e540df8435c9d634db0757762
Alexandra Mikhailovskaia <dokukina.aleksandr@mail.ru>
2023-02-06 21:00:19 +0300

    new commit

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось! Я настроила наконец action')
+    return HttpResponse('У меня получилось! Я настроила action')
 
 
 def second_page(request):
