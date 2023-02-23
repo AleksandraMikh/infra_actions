1a929ad3f889a38f0b4a87ab6405d8399baff975
Alexandra Mikhailovskaia <dokukina.aleksandr@mail.ru>
2023-02-23 17:08:27 +0300

    third test commit

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось! Я настроила action. Наверно')
+    return HttpResponse('У меня получилось! Я настроила action. Точно')
 
 
 def second_page(request):
