f1ef209d3e1bb44e1417bfc380f17200f2090e89
Alexandra Mikhailovskaia <dokukina.aleksandr@mail.ru>
2023-02-24 20:10:41 +0300

    deploy

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось! Я настроила action.ббба')
+    return HttpResponse('У меня получилось! Я настроила action.ddd')
 
 
 def second_page(request):
